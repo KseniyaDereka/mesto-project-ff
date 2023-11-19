3cc7cf41d8d185d239af3197069ebc36c6d82136
Kseniya Dereka <mail@kseniyadereka.com>
2023-11-19 19:09:03 +0300

    feat: реализовать рендер карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,48 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+;
+
+const initialCards = [
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    }
+];
+
+
+function renderCards(item, deleteCard){
+
+ const cardTemplate = document.querySelector('#card-template').content;
+ const cardContainer = document.querySelector('.places__list');
+ 
+ const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+ console.log(cardElement);
+ cardElement.querySelector('.card__image').src = item.link;
+ cardElement.querySelector('.card__title').textContent = item.name;
+ cardContainer.append(cardElement);
+ }
+
+initialCards.forEach((item) => {renderCards(item)});
+
+function deleteCard(){}
