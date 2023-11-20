7cb3cfd2df2e8c75d41306cda010dee1987d8f9c
Kseniya Dereka <mail@kseniyadereka.com>
2023-11-20 23:36:01 +0300

    feat: реализовать открытие попапов и их закрытие на крестик

--- scripts/index.js
+++ scripts/index.js
@@ -7,69 +7,106 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
-;
+//переменные попапов
+const popupEditProfile = document.querySelector(".popup_type_edit");
+const popupAddCard = document.querySelector(".popup_type_new-card");
+const popupZoomImage = document.querySelector(".popup_type_image");
 
+//переменные кнопок
+const openEditPopup = document.querySelector(".profile__edit-button");
+const openAddPopup = document.querySelector(".profile__add-button");
+
+const closeEditPopup = document.querySelector(".button-close_type_edit");
+const closeAddPopup = document.querySelector(".button-close_type_add");
+const closeZoomPopup = document.querySelector(".button-close_type_zoom");
+//Функции попапов
+
+function openPopup(popup) {
+  popup.classList.add("popup_is-opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_is-opened");
+}
+
+//слушатели
+openEditPopup.addEventListener("click", () => {
+  openPopup(popupEditProfile);
+});
+
+openAddPopup.addEventListener("click", () => {
+  openPopup(popupAddCard);
+});
+
+closeEditPopup.addEventListener("click", () => {
+    closePopup(popupEditProfile);
+  });
+
+closeAddPopup.addEventListener("click", () => {
+    closePopup(popupAddCard);
+  });
+
+closeZoomPopup.addEventListener("click", () => {
+    closePopup(popupZoomImage);
+  });
 
 
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
+function createCard(item, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
+  const deleteCardButton = card.querySelector(".card__delete-button");
 
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
 
-function createCard(item, deleteCard){
- const cardTemplate = document.querySelector('#card-template').content;
- const card = cardTemplate.querySelector('.card').cloneNode(true);
- const cardImage = card.querySelector('.card__image');
- const cardTitle = card.querySelector('.card__title');
- const deleteCardButton = card.querySelector('.card__delete-button');
+  deleteCardButton.addEventListener("click", () => {
+    deleteCard(deleteCardButton, ".card");
+  });
 
- cardImage.src = item.link;
- cardImage.alt = item.name;
- cardTitle.textContent = item.name;
+  return card;
+}
 
- deleteCardButton.addEventListener('click', () => { deleteCard(deleteCardButton, '.card') });
-
-return card;
-
- }
-
- function deleteCard(button, selector){
-    const cardParent = button.closest(selector);
-    cardParent.remove();
+function deleteCard(button, selector) {
+  const cardParent = button.closest(selector);
+  cardParent.remove();
 }
 
 function renderCards(item) {
-const newCard = createCard(item, deleteCard);
-const cardContainer = document.querySelector('.places__list');
-cardContainer.append(newCard);
- }
+  const newCard = createCard(item, deleteCard);
+  const cardContainer = document.querySelector(".places__list");
+  cardContainer.append(newCard);
+}
 
- initialCards.forEach((item) => {
-    renderCards(item);
-   });
-
-
+initialCards.forEach((item) => {
+  renderCards(item);
+});
