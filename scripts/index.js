a74c79e1396cdca5ca036385b4512a0b2fb980cd
Kseniya Dereka <mail@kseniyadereka.com>
2023-11-21 15:13:56 +0300

    style: поправить наименования классов

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// @todo: Темплейт карточки
+import { initialCards } from "./cardMassive.js" // @todo: Темплейт карточки
 
 // @todo: DOM узлы
 
@@ -100,33 +100,6 @@ closeZoomPopup.addEventListener("click", () => {
 editFormElement.addEventListener("submit", handleFormSubmit);
 
 addFormElement.addEventListener("submit", addMyCard);
-
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
 
 function createCard(item, handleDeleteCard, handleCardLike, evt) {
   const cardTemplate = document.querySelector("#card-template").content;
